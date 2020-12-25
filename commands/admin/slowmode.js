3e767220de54bd6ab2069eace248b00588010786
Naufal-DevVoder <75933350+Naufal-DevVoder@users.noreply.github.com>
2020-12-25 14:24:34 +0000

    Update slowmode.js

--- commands/admin/slowmode.js
+++ commands/admin/slowmode.js
@@ -2,7 +2,7 @@ const ms = require("ms");
 
 exports.run = async (client, message, args) => {
   if (!message.member.permissions.any(["ADMINISTRATOR", "MANAGE_CHANNELS"])) {
-    return message.channel.send("Oopsie, you don't have any rights to do this.");
+    return message.channel.send("MAAF, Kamu Tidak Memiliki Akses !");
   }
   
   let channel = message.mentions.channels.first(),
@@ -13,21 +13,21 @@ exports.run = async (client, message, args) => {
   
   if (message.flags[0] === "off") {
     channel.setRateLimitPerUser(0);
-    return message.channel.send(`<#${channel.id}> slowmode has been deactivated.`);
+    return message.channel.send(`<#${channel.id}> Slowmode telah dinonaktifkan.`);
   }
   
-  if (!time) return message.channel.send("Please includes the time format.");
+  if (!time) return message.channel.send("Harap sertakan format waktu.");
   
   let convert = ms(time); // This will results the milliseconds.
   let toSecond = Math.floor(convert / 1000); // This will convert the ms to s. (seconds)
   
-  if (!toSecond || toSecond == undefined) return message.channel.send("Please insert the valid time format!");
+  if (!toSecond || toSecond == undefined) return message.channel.send("Tolong Sertakan Waktu Yang Benar !");
   
   if (toSecond > 21600) return message.channel.send("Timer should be less than or equal to 6 hours.");
   else if (toSecond < 1) return message.channel.send("Timer should be more than or equal to 1 second.");
   
   await channel.setRateLimitPerUser(toSecond);
-  return message.channel.send(`This channel: <#${channel.id}> has been slowing down for **${ms(ms(time), {long: true})}**.`);
+  return message.channel.send(`Channel Ini: <#${channel.id}> Berhasil Di SlowMode Selama **${ms(ms(time), {long: true})}**.`);
 }
 
 exports.help = {
