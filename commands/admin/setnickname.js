ca8ce01594de9fda7c03b8745e4889f82f265683
Naufal-DevVoder <75933350+Naufal-DevVoder@users.noreply.github.com>
2020-12-25 14:19:36 +0000

    Update setnickname.js

--- commands/admin/setnickname.js
+++ commands/admin/setnickname.js
@@ -4,26 +4,26 @@ exports.run = async (client, message, args) => {
   
   // You can make a single array to detect the user permissions.
   if (!message.member.hasPermission(["MANAGE_GUILD", "ADMINISTRATOR"])) {
-    return message.channel.send({embed: {color: "RED", description: "You can't use this command!"}})
+    return message.channel.send({embed: {color: "RANDOM", description: "You can't use this command!"}})
   }
   
   let user = message.mentions.users.first(); // You can mention someone, not only just user.
-  if (!user) return message.channel.send({embed: {color: "RED", description: "You need to mention the user!"}});
+  if (!user) return message.channel.send({embed: {color: "RANDOM", description: "Kamu Harus Mention Orangnya Dulu !"}});
   
   let nick = args.slice(1).join(" ");
-  if (!nick) return message.channel.send({embed: {color: "RED", description: "You need to input the nickname!"}});
+  if (!nick) return message.channel.send({embed: {color: "RANDOM", description: "Kamu Harus Ketik Nama Orangnya Dulu !"}});
   
   let member = message.guild.members.cache.get(user.id);
   
-  await member.setNickname(nick).catch(err => message.channel.send({embed: {color: "RED", description: `Error: ${err}`}}));
-  return message.channel.send({embed: {color: "GREEN", description: `Successfully changed **${user.tag}** nickname to **${nick}**`}});
+  await member.setNickname(nick).catch(err => message.channel.send({embed: {color: "RANDOM", description: `Error: ${err}`}}));
+  return message.channel.send({embed: {color: "BLUE", description: `Berhasil Mengganti **${user.tag}** Menjadi **${nick}**`}});
 }
 
 exports.help = {
   name: "setnickname",
   description: "Set a user nickname.",
-  usage: "/setnickname <@user> <nick>",
-  example: "/setnickname @NaufaL-Kun#5555 hoisted"
+  usage: "c/setnickname <@user> <nick>",
+  example: "c/setnickname @NaufaL-Kun#5555 Handsome"
 }
 
 exports.conf = {
